a0297dfaa40eea1f4c33a4a996b5e891392a1ddf
joybow <juntwolead2@gmail.com>
2023-10-04 00:09:16 +0000

    05 create login system

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.category_id = 1
+    @product.save
+    redirect_to product_url @product
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+    def product_params
+      params.require(:product).permit(:name, :discription, :price)
+    end
+end
